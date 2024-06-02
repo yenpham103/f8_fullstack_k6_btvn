a310483fd85ed1f55c479d727690d76ebad16a89
phamyen <nhovequehuong1003@gmail.com>
2024-06-02 12:53:09 +0700

    up

--- Day37/js/app.js
+++ Day37/js/app.js
@@ -8,6 +8,7 @@ const restartBtn = infoBox.querySelector(".buttons .restart");
 const quizBox = document.querySelector(".quiz-box");
 const timeCount = quizBox.querySelector(".time-sec");
 const timeLine = quizBox.querySelector(".time-line");
+const timeOff = quizBox.querySelector(".time-text");
 
 const nextBtn = document.querySelector(".next-btn");
 const section = document.querySelector("section");
@@ -41,7 +42,7 @@ restartBtn.addEventListener("click", () => {
   infoBox.classList.remove("active");
   quizBox.classList.add("active");
   getQuests(questCount);
-  startTime(timeValue);
+  // startTime(timeValue);
   startTimeLine(widthValue);
 });
 
@@ -51,16 +52,20 @@ nextBtn.addEventListener("click", async function () {
     questCount++;
     getQuests(questCount);
     clearInterval(counter);
-    startTime(timeValue);
     clearInterval(counterLine);
     startTimeLine(timeValue);
     nextBtn.style.display = "none";
+    timeOff.innerText = "Time Left";
   } else {
+    clearInterval(counter);
+    clearInterval(counterLine);
     showResultBox();
   }
+  if (questCount === data.length - 1)
+    return (nextBtn.innerText = "View Result");
 });
 
-const renderQuest = ({ number, question, answer, options }) => {
+const renderQuest = ({ number, question, options }) => {
   //   const optionList = section.querySelector(".option-list");
   const totalQuest = document.querySelector(".total-que");
   const indexNow = totalQuest.children[0].children[0];
@@ -83,8 +88,6 @@ const renderQuest = ({ number, question, answer, options }) => {
   </div>
 </div>`;
   section.innerHTML = htmlQuiz;
-  //   const option = optionList.querySelectorAll(".option");
-  //   console.log(option);
 };
 
 const getQuests = async (quest) => {
@@ -103,13 +106,16 @@ const getQuest = async () => {
   const quests = await response.json();
   return quests;
 };
+
 let iconTick = `<div class="icon tick">
 <i class="fa-solid fa-check"></i></div>`;
 let iconCross = `<div class="icon cross">
 <i class="fa-solid fa-xmark"></i></div>`;
+
 function handleAnswer({ answer }) {
   const listOption = document.querySelector(".option-list");
   const options = listOption.querySelectorAll(".option");
+  startTime(timeValue, answer, options);
   const correctAnswer = String(answer).trim().toLowerCase();
   options.forEach((option, index) => {
     option.addEventListener("click", function (e) {
@@ -144,21 +150,6 @@ function showResultBox() {
   const scoreText = resultBox.querySelector(".score-text");
   scoreText.innerText = userScore;
 }
-function startTime(time) {
-  counter = setInterval(timer, 1000);
-  function timer() {
-    timeCount.innerText = time;
-    time--;
-    if (time < 9) {
-      let addZero = timeCount.innerText;
-      timeCount.innerText = "0" + addZero;
-    }
-    if (time < 0) {
-      clearInterval(counter);
-      timeCount.innerText = "00";
-    }
-  }
-}
 function startTimeLine(time) {
   counterLine = setInterval(timer, 29);
   function timer() {
@@ -166,6 +157,33 @@ function startTimeLine(time) {
     timeLine.style.width = time + "px";
     if (time > 549) {
       clearInterval(counterLine);
+    }
+  }
+}
+function startTime(time, answer, options) {
+  counter = setInterval(timer, 1000);
+  async function timer() {
+    timeCount.innerText = time;
+    time--;
+    if (time < 9) {
+      let addZero = timeCount.innerText;
+      timeCount.innerText = "0" + addZero;
+    }
+    if (time < 0) {
+      const correctAnswer = String(answer).trim().toLowerCase();
+      clearInterval(counter);
+      timeCount.innerText = "00";
+      timeOff.innerText = "Time Off";
+      options.forEach((option) => {
+        const userAnswer = String(option.innerText).trim().toLowerCase();
+        if (userAnswer === correctAnswer) {
+          option.setAttribute("class", "option correct");
+        }
+      });
+      options.forEach((option) => {
+        option.classList.add("disabled");
+      });
+      nextBtn.style.display = "block";
     }
   }
 }
